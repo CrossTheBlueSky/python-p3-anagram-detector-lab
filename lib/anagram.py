b345707c1d62f21ee2847731e23be9f72b6e9c69
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-12 14:34:35 -0700

    tests passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        out = []
+        for entry in list:
+            if sorted(entry) == sorted(self.word):
+                out.append(entry)
+        return out
